933d65b46080470492943df8cf1fc358aa5e6322
sharon <sharon329@naver.com>
2025-02-10 15:06:41 +0900

    ✨ Feat(inlineFunction) 사용자와 상호작용 하는 컴포넌트

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,7 +4,9 @@ import './Square.css'
 export default class Square extends Component {
   render() {
     return (
-      <button className='square'>
+      <button 
+        className='square'
+        onClick={() => console.log(`click num : ${this.props.value}`)}>
         {this.props.value}
       </button>
     )
