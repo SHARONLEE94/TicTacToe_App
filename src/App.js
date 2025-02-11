aa7bb35f84fae63658bff6e95ee8d5ef465e75ce
sharon <sharon329@naver.com>
2025-02-11 15:48:36 +0900

    ✨ Feat(historyDelete) 이전 기록 삭제 기능 추가

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
 
   const [history, setHistory] = useState([{squares: Array(9).fill(null)}]);
   const [xIsNext, setXIsNext] = useState(true);
+  const [stepNumber, setStepNumber] = useState(0);
+  const current = history[stepNumber]; // 되돌린 이후 기록 삭제
 
   const calculateWinner = (squares) => {
     const lines = [
@@ -28,7 +30,7 @@ function App() {
     return null;
   }
 
-  const current = history[history.length-1];
+  
   const winner = calculateWinner(current.squares);
   let status;
   if(winner) {
@@ -38,13 +40,19 @@ function App() {
   }
 
   const handleClick = (i) => {
-    const newSquares = current.squares.slice();
+    const newHistory = history.slice(0, stepNumber+1);
+    const newCurrent = newHistory[newHistory.length-1];
+    const newSquares = newCurrent.squares.slice();
+
     if(calculateWinner(newSquares) || newSquares[i]) {
       return // → 게임이 끝났거나, 이미 클릭된 칸이면 더 이상 진행하지 않고 반환(return)한다
     }
+
     newSquares[i]  = xIsNext ? 'X' : 'O';
-    setHistory([...history, {squares: newSquares}]);
+    setHistory([...newHistory, {squares: newSquares}]);
     setXIsNext(current => !current);
+
+    setStepNumber(newHistory.length);
   }
 
   const moves = history.map((step, move) => {
@@ -53,10 +61,15 @@ function App() {
       : 'Go to game start'
     return(
       <li key={move}>
-        <button>{desc}</button>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
       </li>
     )
   })
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext((step % 2) === 0);
+  }
 
   return (
     <div className="game">
