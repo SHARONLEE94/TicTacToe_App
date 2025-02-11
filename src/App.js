e958419ce5aa515c9d4484cf6c7417b9156b5f82
sharon <sharon329@naver.com>
2025-02-11 13:55:28 +0900

    ♻️ Refactor(history) 히스토리 기능 추가를 위한 리펙토링
    board 컴포넌트의 기능을 APP 컴포넌트로 이동

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
+import { useState } from "react";
 import "./App.css"
 import Board from "./components/Board";
 
 function App() {
+
+  const [history, setHistory] = useState([{squares: Array(9).fill(null)}]);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],
+    ]
+
+    for(let index = 0; index < lines.length; index++) {
+      const [a,b,c] = lines[index];
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  const current = history[history.length-1];
+  const winner = calculateWinner(current.squares);
+  let status;
+  if(winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  }
+
+  const handleClick = (i) => {
+    const newSquares = current.squares.slice();
+    if(calculateWinner(newSquares) || newSquares[i]) {
+      return // → 게임이 끝났거나, 이미 클릭된 칸이면 더 이상 진행하지 않고 반환(return)한다
+    }
+    newSquares[i]  = xIsNext ? 'X' : 'O';
+    setHistory([...history, {squares: newSquares}]);
+    setXIsNext(current => !current);
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board 
+          squares={ current.squares }
+          onClick={ (i) => {handleClick(i)} }/>
       </div>
-      <div className="game-info">game info</div>
+      <div className="game-info">
+        <div>{status}</div>
+        <div>{/* todo */}</div>
+      </div>
     </div>
   );
 }
--- src/components/Board.js
+++ src/components/Board.js
@@ -2,58 +2,17 @@ import React, { useState } from 'react'
 import Square from './Square';
 import './Board.css'
 
-const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-
-  const calculateWinner = (squares) => {
-    const lines = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6],
-    ]
-
-    for(let index = 0; index < lines.length; index++) {
-      const [a,b,c] = lines[index];
-      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
-  const winner = calculateWinner(squares);
-  let status;
-  if(winner) {
-    status = 'Winner: ' + winner;
-  } else {
-    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
-  }
-
-  const handleClick = (i) => {
-    const newSquares = squares.slice();
-    if(calculateWinner(newSquares) || newSquares[i]) {
-      return // → 게임이 끝났거나, 이미 클릭된 칸이면 더 이상 진행하지 않고 반환(return)한다
-    }
-    newSquares[i]  = xIsNext ? 'X' : 'O';
-    setSquares(newSquares);
-    setXIsNext(prev => !prev); // = (유사) setXIsNext(!xIsNext)
-  }
+const Board = ({squares, onClick}) => {
 
   const renderSquare = (i) => {
     return <Square 
               value = {squares[i]}
-              onClick = {() => {handleClick(i)}} />
+              onClick = {() => {onClick(i)}} />
   }
 
   return (
     <div>
-      <div className='status'>{status}</div>
+      {/* <div className='status'>{status}</div> */}
       <div className='board-row'>
         {renderSquare(0)}
         {renderSquare(1)}
